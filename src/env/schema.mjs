b0a795e0e1669673330709eeff1b4f186cd8d9d4
Kyle McKell <me@kylemckell.com>
2022-08-21 01:24:35 -0600

    added backk nextauth items

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -8,6 +8,10 @@ import { z } from 'zod';
 export const serverSchema = z.object({
 	DATABASE_URL: z.string().url(),
 	NODE_ENV: z.enum(['development', 'test', 'production']),
+	NEXTAUTH_SECRET: z.string(),
+	NEXTAUTH_URL: z.string().url(),
+	GITHUB_ID: z.string(),
+	GITHUB_SECRET: z.string(),
 });
 
 /**
